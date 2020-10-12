fd8aea92489444fa0f0cdcb8171370e559755f48
Ezaz Ahmed <ahmedezaz@users.noreply.github.com>
2020-10-13 02:33:43 +0600

    Update app.js

--- app.js
+++ app.js
@@ -52,3 +52,29 @@ const filterTodos = (term) => {
 	const term = search.value.trim().toLowerCase();
 	filterTodos(term);
 });
+
+ /*Digital Clock*/
+
+ const clock = document.querySelector('.clock');
+
+
+const tick = ()=> {
+	const now = new Date();
+
+	
+
+	const h = now.getHours();
+	const m = now.getMinutes();
+	const s = now.getSeconds();
+	
+	const html = `
+	<span>${h}</span> :
+	<span>${m}</span> :
+	<span>${s}</span> 
+	`;
+
+	clock.innerHTML = html; 
+};
+
+
+setInterval(tick, 1000);
