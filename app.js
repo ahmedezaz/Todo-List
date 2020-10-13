bfbc3318c6430ea082f02c384d6b48a8515d59e2
Ezaz Ahmed <ahmedezaz@users.noreply.github.com>
2020-10-13 23:29:40 +0600

    Update app.js

--- app.js
+++ app.js
@@ -78,3 +78,15 @@ const tick = ()=> {
 
 
 setInterval(tick, 1000);
+/* Wow js code*/
+
+wow = new WOW(
+              {
+              boxClass:     'wow',      // default
+              animateClass: 'animated', // default
+              offset:       0,          // default
+              mobile:       true,       // default
+              live:         true        // default
+            }
+            )
+            wow.init();
